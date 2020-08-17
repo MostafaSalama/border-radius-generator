e8b87e467d9db066e75d095d3507b5186a43386f
Mostafa Salama <mostafatourad789@gmail.com>
2020-08-17 02:14:22 +0200

    add box component to change its radius

---
+++ src/components/Box.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './box.module.css'
+
+const Box = ({})=>{
+    return (
+       <div className={styles.container}>
+           <div className={styles.box}>
+           </div>
+       </div>
+    )
+}
+export default Box ;
