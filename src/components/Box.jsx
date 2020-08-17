759c047f7ad721780a2d19f0254b06594a5228b5
Mostafa Salama <mostafatourad789@gmail.com>
2020-08-17 02:44:02 +0200

    update box component to change border radius when context values change

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import styles from './box.module.css'
 import InputRange from "./InputRange";
+import BorderContext from "../app-context";
 
-const Box = ({})=>{
+const Box = ()=>{
+    const {borderValues} = useContext(BorderContext) ;
+    const borderRadius = borderValues.join(' ') ;
+    console.log(borderRadius)
     return (
        <div className={styles.container}>
-           <div className={styles.box}>
+           <div className={styles.box} style={{borderRadius}}>
            </div>
        </div>
     )
