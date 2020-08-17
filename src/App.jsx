445088fc35e9f1f55cd77b2f7aace2ee68314169
Mostafa Salama <mostafatourad789@gmail.com>
2020-08-17 02:53:39 +0200

    update main app component

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 import Box from "./components/Box";
 import BorderContext from "./app-context";
+import InputRangeContainer from "./components/InputRangeContainer";
+import BorderBoxText from "./components/BorderBoxText";
 
 function App() {
   const [borderValues,updateBorderValues] = useState(['50%','50%','50%','50%']);
-  function updateBorderIndex(index,value) {
+  function updateBorderAtIndex(index,value) {
         const newBorderValues = [...borderValues] ;
         newBorderValues[index] = value;
         updateBorderValues(newBorderValues)
   }
   return (
-    <div>
-      <BorderContext.Provider value={{borderValues,updateBorderValues}}>
-
+    <div className='app'>
+      <BorderContext.Provider value={{borderValues,updateBorderAtIndex}}>
+          <Box/>
+          <InputRangeContainer/>
+          <BorderBoxText/>
       </BorderContext.Provider>
     </div>
   );
